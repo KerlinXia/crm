11c0b7d9f1d9b2a0a260520dcd5131f754314761
Kerlin <mazhao_222@163.com>
2021-05-09 21:38:12 +0800

    conmmit 单元测试

---
+++ src/main/java/cn/kerlin/crm/controller/DepartmentController.java
@@ -0,0 +1,31 @@
+package cn.kerlin.crm.controller;
+
+import cn.kerlin.crm.domain.Department;
+import cn.kerlin.crm.service.IDepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author:kerlin
+ * @Date
+ */
+@Controller
+@RequestMapping("/department")
+public class DepartmentController {
+
+    @Autowired
+    private IDepartmentService departmentService;
+
+    //查询所有
+    @RequestMapping("/selectAll")
+    public String selectAll(Model model){
+        List<Department>list = departmentService.selectAllService();
+        model.addAttribute("departmentlist",list);
+        return "index.html";
+    }
+}
