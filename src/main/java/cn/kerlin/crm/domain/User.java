b251eeacb781ff774c8d8097b95a8a2a40679971
Kerlin <mazhao_222@163.com>
2021-05-09 15:47:07 +0800

    conmmit test2

---
+++ src/main/java/cn/kerlin/crm/domain/User.java
@@ -0,0 +1,19 @@
+package cn.kerlin.crm.domain;
+
+/**
+ * @Description:
+ * @Author:kerlin
+ * @Date
+ */
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
